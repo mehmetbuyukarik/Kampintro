fb3ebdf4edf91a5a6f3818752d6f71a4cfeae96c
Mehmet Büyükarık <mbuyukarik@gmail.com>
2021-04-24 19:15:00 +0300

    24 nisan ders sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string urunAdi = "Elma";
+            //double fiyati = 15;
+            //string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyatı = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "KArpuz";
+            urun2.Fiyatı = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyatı);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------------------");
+
+                
+
+            }
+
+            Console.WriteLine("---------Metotlar--------------");
+
+            SepetManager sepetmanager = new SepetManager();
+            sepetmanager.Ekle(urun1);
+            sepetmanager.Ekle(urun2);
+            sepetmanager.Ekle2("Armut", "Yeşil Armut", 12 ,10);
+            sepetmanager.Ekle2("elma", "Yeşil Armut", 12 ,15);
+            sepetmanager.Ekle2("karpuz", "diyarbakır karpuz", 12,10);
+
+        }
+    }
+}
+// Do not repeat yourself - Dry - Clean Code - Best Practice
---
+++ Odev1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.productName = "Semih";
+            product1.productQuantity = 50;
+            product1.productPrice = 100;
+
+            Product product2 = new Product();
+
+            product2.productName = "Mehmet ";
+            product2.productQuantity = 60;
+            product2.productPrice = 80;
+
+            Product product3 = new Product();
+
+            product3.productName = "Merve";
+            product3.productQuantity = 40;
+            product3.productPrice = 90;
+
+            Product[] Products = new Product[] {product1,product2,product3 };
+
+            foreach (var product in Products)
+            {
+                Console.WriteLine(product.productName + ":" + product.productQuantity + " - " + product.productPrice);
+            }
+
+
+        }
+    }
+
+    class Product
+    {
+        public  string productName { get; set; }
+        public  int productQuantity { get; set; }
+        public double productPrice { get; set; }
+    }
+}
